ead9706421f21459b3279e34291401823d1404db
adelhpour <ad.heydarabadi@gmail.com>
2024-03-07 00:45:24 -0800

    _layout_is_specified and _render_is_specified are moved to libsbmlnetworkeditor

--- src/sbmlnetworkeditor/sbmlnetworkeditor.py
+++ src/sbmlnetworkeditor/sbmlnetworkeditor.py
@@ -16,24 +16,10 @@ class SBMLNetworkEditor(libsbmlnetworkeditor.LibSBMLNetworkEditor):
         :param file_name:
         :param file_format:
         """
-        if not self._layout_is_specified() or not self._render_is_specified():
-            self.autolayout()
         if file_directory:
             networkinfotranslator.import_sbml_export_figure(self.export(), file_directory, file_name, file_format)
         else:
             display(networkinfotranslator.import_sbml_export_pil_image(self.export()))
-
-    def _layout_is_specified(self):
-        if self.getNumLayouts():
-            return True
-
-        return False
-
-    def _render_is_specified(self):
-        if self.getNumGlobalRenderInformation() or self.getNumLocalRenderInformation():
-            return True
-
-        return False
 
 
 
